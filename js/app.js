60bd29ea424343d350b6ede8c80b9921e3dfeb74
Paul Jones <paulvjones@gmail.com>
2017-01-25 10:09:09 -0700

    stuff

--- js/app.js
+++ js/app.js
@@ -11,12 +11,12 @@ angular.module('nasaViewer', ['ui.router'])
         url: '/apod',
         controller: 'apodContr',
         templateUrl: '../views/apod.html'
-      })
-      .state('apodByDate', {
-        url: '/apod-by-date',
-        controller: 'apodContr',
-        temmplateUrl: '..views/apod-by-date'
-      })
+      });
+      // .state('apodByDate', {
+      //   url: '/apod-by-date',
+      //   controller: 'apodContr',
+      //   temmplateUrl: '../views/apod-by-date.html'
+      // })
 
       $urlRouterProvider
         .otherwise('/')
