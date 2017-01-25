06dbb9e3e21ac9fe22fe77e4a541f4209bc2f089
Paul Jones <paulvjones@gmail.com>
2017-01-24 22:09:53 -0700

    working dashboard widgets for sky view and time, apod viewer page working

--- js/apod/apodContr.js
+++ js/apod/apodContr.js
@@ -1,8 +1,34 @@
 angular.module('nasaViewer').controller('apodContr', function($scope, apodServ) {
 
- apodServ.getCurrentApod().then(function(response) {
+  $scope.apodYears = [];
+  $scope.apodMonths = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+  $scope.apodDays = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"];
+
+  for (var i = 1996; i <= (new Date().getFullYear()); i++) {
+    $scope.apodYears.push(i);
+  };
+
+
+  apodServ.getCurrentApod().then(function(response) {
     $scope.currentApod = response.data;
-    console.log(response.data);
-    
   });
+
+  $scope.getCurrentApod = function() {
+    apodServ.getCurrentApod().then(function(response) {
+      $scope.currentApod = response.data;
+      $scope.yyyy = '';
+      $scope.mm = '';
+      $scope.dd = '';
+    })
+  }
+
+  $scope.getApodByDate = function(yyyy, mm, dd) {
+    var queryDate = (yyyy + "-" + mm + "-" + dd).toString();
+    apodServ.getApodByDate(queryDate).then(function(response) {
+      $scope.currentApod = response.data;
+    })
+  }
+
+
+
 });
--- js/mainContr.js
+++ js/mainContr.js
@@ -1,9 +1,9 @@
-angular.module('nasaViewer').controller('mainContr', function($scope, apodServ) {
+angular.module('nasaViewer').controller('mainContr', function($scope, apodServ, geolocationFact, weatherServ) {
 
-//default background image (set because sometimes images[randomCount] below is evaluating undefined)
+  //default background image (set because sometimes images[randomCount] below is evaluating undefined)
   $scope.bgUrl = {
-        'background-image': "url('../images/home/bg-01.jpg')"
-      } 
+    'background-image': "url('../images/home/bg-01.jpg')"
+  }
 
   $scope.setBgImage = function() {
     var imgCount = 4;
@@ -26,6 +26,35 @@ angular.module('nasaViewer').controller('mainContr', function($scope, apodServ) 
 
   apodServ.getCurrentApod().then(function(response) {
     $scope.currentApod = response.data;
+  });
+
+
+
+  geolocationFact.getCurrentPosition().then(function(response) {
+    var userLat = response.coords.latitude;
+    var userLong = response.coords.longitude;
+    weatherServ.getWeather(userLat, userLong).then(function(response) {
+      var weatherObj = response.data.currently;
+      $scope.cloudCover = weatherObj.cloudCover;
+      $scope.weatherSummary = weatherObj.summary;
+      $scope.visibility = weatherObj.visibility;
+      $scope.temperature = weatherObj.temperature;
+
+
+      var skyView = ""
+      if (weatherObj.cloudCover <= 0.1 && weatherObj.visibility >= 9) {
+        skyView = "excellent";
+      } else if (weatherObj.cloudCover <= 0.2 || weatherObj.visibility >= 5) {
+        skyView = "good";
+      } else if (weatherObj.cloudCover <= 0.3 || weatherObj.visibility >= 3) {
+        skyView = "fair";
+      } else if (weatherObj.cloudCover <= 0.5 || weatherObj.visibility >= 1) {
+        skyView = "poor";
+      }
+
+      $scope.skyView = skyView;
+      console.log($scope.skyView);
+    })
   });
 
 
